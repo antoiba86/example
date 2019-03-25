72896499c503bfd2d6c9d66effac8e798a35d5f0
antoiba86 <ajibanez@suratica.es>
2019-03-25 20:31:14 +0100

    Added guarded property to candidate and job models

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     protected $table = "candidates";
+    protected $guarded= [];
 
     /**
      * Get the jobs for a candidate.
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $table = "jobs";
+    protected $guarded= [];
 
     /**
      * Get the candidate of a job
